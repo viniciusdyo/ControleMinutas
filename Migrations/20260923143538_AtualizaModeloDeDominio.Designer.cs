4470b1f2cd965c371610b76eca0614396f4e11ee
Vinicius Dyonisio <viniciusdyo@gmail.com>
2026-09-23 11:37:53 -0300

    feat: Cria componentes para Formulários de registro, Input personalizado para moeda local (reais). fix: Implementa método herdado de InputBase no componente InputText; refact: Cria migração que atualiza o modelo de domínio no DB.

--- Migrations/20260923143538_AtualizaModeloDeDominio.Designer.cs
+++ Migrations/20260923143538_AtualizaModeloDeDominio.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ControleMinutas.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ControleMinutas.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260923143538_AtualizaModeloDeDominio")]
+    partial class AtualizaModeloDeDominio
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.12");
