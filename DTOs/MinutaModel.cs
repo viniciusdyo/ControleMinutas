6bf9f6fb962e6364741dbeecc8a1164971ad5dbb
Vinicius Dyonisio <viniciusdyo@gmail.com>
2026-09-23 12:19:23 -0300

    fix: Altera assinatura de métodos dos componentes Form; Remove definição de propriedade referente ao OnValidSubmit que é um Callback nativo do Blazor.

---
+++ DTOs/MinutaModel.cs
@@ -0,0 +1,12 @@
+﻿using ControleMinutas.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleMinutas.DTOs;
+
+public class MinutaModel
+{
+    [Required]
+    public int TrabalhoId { get; set; }
+    [Required]
+    public decimal Valor { get; set; }
+}
