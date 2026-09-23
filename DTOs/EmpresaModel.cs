e866e8d120dff46f12b8a1bba760d8c3aae6a70c
Vinicius Dyonisio <viniciusdyo@gmail.com>
2026-09-23 15:55:31 -0300

    feat: Cria modelos iniciais para Empresa e Trabalho;

---
+++ DTOs/EmpresaModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleMinutas.DTOs;
+
+public class EmpresaModel
+{
+    public string Nome { get; set; } = string.Empty;
+    public decimal TaxaAbastecimento { get;  set; }
+    public decimal TaxaTroca { get; set; }
+
+}
---
+++ DTOs/TrabalhoModel.cs
@@ -0,0 +1,11 @@
+﻿using ControleMinutas.Entities;
+
+namespace ControleMinutas.DTOs;
+
+public class TrabalhoModel
+{
+    public int EmpresaId { get; private set; }
+    public int SaidaId { get; private set; }
+    public int EntregaId { get; private set; }
+    public decimal Valor { get; private set; }
+}
